ae806d258068337c80af59290d8db1923194e2d3
Matteo Boschi <mbmaken@gmail.com>
2019-06-06 21:30:32 +0200

    refactor code from review

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,20 +1,23 @@
-type AsyncFunc<T, R> = (item: T) => Promise<R>;
-
 /**
  * applies asyncFunc to each item and returns a promise with the array of results
  * @param items
  * @param asyncFunc
- * @param initialValue
  */
 export function sequential<T, R>(
   items: ReadonlyArray<T>,
-  asyncFunc: AsyncFunc<T, R>,
-  initialValue: ReadonlyArray<R>
+  asyncFunc: (item: T) => Promise<R>
 ): Promise<ReadonlyArray<R>> {
-  return items.reduce((prom, item) => {
-    return prom.then(async acc => {
-      const result = await asyncFunc(item);
-      return [...acc, result];
-    });
-  }, Promise.resolve(initialValue)); // initial
+  return items.reduce(
+    (prom, item) => prom.then(async acc => [...acc, await asyncFunc(item)]),
+    Promise.resolve([] as ReadonlyArray<R>)
+  );
+}
+
+export async function sequentialSum<T>(
+  items: ReadonlyArray<T>,
+  asyncFunc: (item: T) => Promise<number>
+): Promise<number> {
+  return sequential(items, asyncFunc).then(_ =>
+    _.reduce((acc, curr) => acc + curr)
+  );
 }
