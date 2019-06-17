4963c5276f4de2f42e12c896e720c2ba30b4c701
Matteo Boschi <mbmaken@gmail.com>
2019-06-17 18:52:08 +0200

    Revert "fix tests"
    This reverts commit 7eef69175b213fb5c727ab4bd4bcdd32e7e518dc.

--- test/commands/users/list.test.ts
+++ test/commands/users/list.test.ts
@@ -1,29 +1,17 @@
-import { expect, test } from "@oclif/test";
-import { hasCosmosConnection } from "../../../src/utils/azure";
+import {expect, test} from '@oclif/test'
 
-describe("list all profiles", () => {
-  const isCosmosConnectionAvailable = hasCosmosConnection(
-    "agid-rg-test",
-    "agid-cosmosdb-test"
-  );
-  /**
-   * if host has the az client installed and valid cosmos credentials we test command first output line matches
-   * the header row (fiscalCode column name). Otherwise the test expects for an error with 2 as exit status
-   */
-  if (isCosmosConnectionAvailable) {
-    test
-      .stdout()
-      .command(["profiles:list"])
-      .it("runs profiles command to list all users", ctx => {
-        expect(ctx.stdout).match(/^fiscalCode/);
-      });
-  } else {
-    test
-      .stdout()
-      .command(["profiles:list"])
-      .exit(2)
-      .it(
-        "check the command goes in error (no cosmos credential available or az client not installed)"
-      );
-  }
-});
+describe('users:list', () => {
+  test
+    .stdout()
+    .command(['users:list'])
+    .it('runs hello', ctx => {
+      expect(ctx.stdout).to.contain('hello world')
+    })
+
+  test
+    .stdout()
+    .command(['users:list', '--name', 'jeff'])
+    .it('runs hello --name jeff', ctx => {
+      expect(ctx.stdout).to.contain('hello jeff')
+    })
+})
