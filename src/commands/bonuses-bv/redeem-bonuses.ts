b998e2d43394f4c29f53560221bd138398bfe752
pasqualedevita <>
2020-09-23 09:15:08 +0200

    reprocess redeemed requests without save requests as local file and others fix

--- src/commands/bonuses-bv/redeem-bonuses.ts
+++ src/commands/bonuses-bv/redeem-bonuses.ts
@@ -2,41 +2,32 @@ import { Command, flags } from "@oclif/command";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { pickAzureConfig, getStorageConnection } from "../../utils/azure";
 import fetch from "node-fetch";
-import * as fs from "fs";
-import * as rimraf from "rimraf";
 import cli from "cli-ux";
 
 interface OutputResult {
-  file: string;
+  redeemedRequest: string;
   statusCode: string;
 }
 
 export default class RedeemBonuses extends Command {
-  public static description = "Download and reprocess redeemed bonuses";
+  public static description = "Reprocess redeemed bonuses in the specified day";
 
   public static args = [];
 
   public static flags = {
-    tmpDir: flags.string({
-      char: "o",
-      default: "tmp",
-      description: "temp directory",
-      required: true
-    }),
-    containerFolder: flags.string({
-      char: "c",
-      description: "container folder",
+    day: flags.string({
+      char: "d",
+      description: "day",
       required: true
     }),
     apiUrl: flags.string({
       char: "u",
       default: "https://api-gad.io.italia.it/api/bonus-vacanze/v1/redeemed",
-      description: "api url",
-      required: true
+      description: "bonus vacanze api url"
     }),
     apiKey: flags.string({
       char: "k",
-      description: "api key",
+      description: "bonus vacanze api key",
       required: true
     })
   };
@@ -44,77 +35,71 @@ export default class RedeemBonuses extends Command {
   public async run(): Promise<void> {
     const { flags: parsedFlags } = this.parse(RedeemBonuses);
 
-    let redeemedRequests = Array<string>();
-    let outputResults = Array<OutputResult>();
-
     try {
+      // get access to resources
       const config = await pickAzureConfig();
-
       const storageConnection = await getStorageConnection(config.storageName);
-
       const blobServiceClient = BlobServiceClient.fromConnectionString(
         storageConnection
       );
-
       const containerClient = blobServiceClient.getContainerClient(
         config.storageBonusRedeemedContainer
       );
 
-      if (
-        fs.existsSync(parsedFlags.tmpDir + "/" + parsedFlags.containerFolder)
-      ) {
-        rimraf.sync(parsedFlags.tmpDir + "/" + parsedFlags.containerFolder);
-      }
-      fs.mkdirSync(parsedFlags.tmpDir + "/" + parsedFlags.containerFolder, {
-        recursive: true
-      });
+      cli.action.start("Reprocessing redeemed requests...");
 
-      this.log("\nListing blobs...");
-      cli.action.start("Downloading redeemed requests started...");
-      // List and download the blob(s) in the container.
+      // array where we save reprocessed redeemed requests and response status codes
+      let outputResults = Array<OutputResult>();
+
+      // get and reprocess redeemed requests
       for await (const blob of containerClient.listBlobsFlat({
-        prefix: parsedFlags.containerFolder
+        prefix: parsedFlags.day
       })) {
-        this.log("\t", blob.name);
-        const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
-        const response = await blockBlobClient.downloadToFile(
-          parsedFlags.tmpDir + "/" + blob.name
-        );
-        redeemedRequests.push(parsedFlags.tmpDir + "/" + blob.name);
-      }
-      cli.action.stop("Downloading redeemed requests finished...");
+        console.log("\t", blob.name);
 
-      cli.action.start("Sending redeemed requests started...");
-      // Send redeemed requests
-      for (const redeemedRequest of redeemedRequests) {
-        this.log(redeemedRequest);
-        const rawDataRequest = fs.readFileSync(
-          redeemedRequest,
-          "utf8"
-        ) as string;
-        const rawDataResponse = await fetch(parsedFlags.apiUrl, {
-          method: "POST",
-          headers: {
-            "Ocp-Apim-Subscription-Key": parsedFlags.apiKey,
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(rawDataRequest)
-        });
-        const content = await rawDataResponse.json();
-        outputResults.push({
-          file: redeemedRequest,
-          statusCode: rawDataResponse.status.toString()
-        });
-        // pause 1 second to avoid 429 statusCode response
-        await this.delay(1000);
+        // inizialize outputResult
+        let outputResult = {
+          redeemedRequest: blob.name,
+          statusCode: "0"
+        };
+
+        const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
+        const downloadBlockBlobResponse = await blockBlobClient.download(0);
+
+        if (downloadBlockBlobResponse.readableStreamBody != undefined) {
+          // get redeemed request content
+          const rawDataRequest = await this.streamToString(
+            downloadBlockBlobResponse.readableStreamBody
+          );
+
+          // do redeem request
+          const rawDataResponse = await fetch(parsedFlags.apiUrl, {
+            method: "POST",
+            headers: {
+              "Ocp-Apim-Subscription-Key": parsedFlags.apiKey,
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(rawDataRequest)
+          });
+
+          // save response status code
+          outputResult.statusCode = rawDataResponse.status.toString();
+
+          // pause 10 seconds to avoid to flood the backend server and 429 statusCode response
+          await this.delay(10 * 1000);
+        }
+
+        // push reprocessed request in results array
+        outputResults.push(outputResult);
       }
-      cli.action.stop("Sending redeemed requests finished...");
+
+      cli.action.stop();
 
       cli.table(
         outputResults,
         {
-          file: {
-            header: "file"
+          redeemedRequest: {
+            header: "redeemedRequest"
           },
           statusCode: {
             header: "statusCode"
@@ -130,7 +115,22 @@ export default class RedeemBonuses extends Command {
     }
   }
 
+  // A helper function used to add a pause in the flow
   private async delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  // A helper function used to read a Node.js readable stream into a string
+  private async streamToString(readableStream: NodeJS.ReadableStream) {
+    return new Promise((resolve, reject) => {
+      const chunks: any[] = [];
+      readableStream.on("data", data => {
+        chunks.push(data.toString());
+      });
+      readableStream.on("end", () => {
+        resolve(chunks.join(""));
+      });
+      readableStream.on("error", reject);
+    });
   }
 }
